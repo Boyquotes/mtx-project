e3c97c3f9410fa8a48a5ef353fb0763efba9f964
Маркозов Николай <sonicboomchan@mail.ru>
2023-02-18 01:24:06 +0100

    some ui changes

--- src/scenes/TitleScreen.gd
+++ src/scenes/TitleScreen.gd
@@ -5,7 +5,7 @@ export var unitBuyScreen : PackedScene
 
 
 func _on_PlayButton_button_up():
-	get_tree().change_scene("res://src/scenes/UnitSelectScreen.tscn")
+	get_tree().change_scene("res://src/scenes/select_scene/UnitSelectScreen.tscn")
 
 
 func _on_BuyUnitButton_button_up():
